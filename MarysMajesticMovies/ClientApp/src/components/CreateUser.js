3f63fa06f2b12c25500a2e361caf197bcccbc33c
SofiaLindgren <sofia.lindgren@iths.se>
2020-04-16 12:09:23 +0200

    WEB3-62 Implemented simple design

--- MarysMajesticMovies/ClientApp/src/components/CreateUser.js
+++ MarysMajesticMovies/ClientApp/src/components/CreateUser.js
@@ -1,9 +1,10 @@
-﻿import React from 'react';
+﻿import React, { Component } from 'react';
 
 export class CreateUser extends Component {
     static displayName = CreateUser.name;
 
-    constructor(props) {
+    constructor() {
+        super();
     }
 
     render() {
@@ -13,23 +14,23 @@ export class CreateUser extends Component {
                 <div class="register">
                     <h1> Create an account </h1>
                     <form>
-                        <label for="fname">Name</label><br>
-                        <input type="text" id="fname" name="fname" placeholder="First name*"><br>
-                        <input type="text" id="lname" name="lname" placeholder="Last name*"><br><br>
+                        <label for="fname">Name</label><br />
+                        <input type="text" id="fname" name="fname" placeholder="First name*" /><br />
+                        <input type="text" id="lname" name="lname" placeholder="Last name*" /><br /><br />
 
-                        <label for="fname">Billing and shippingadress</label><br>
-                        <input type="text" id="adress" name="adress" placeholder="Adress*"><br>
-                        <input type="text" id="city" name="city" placeholder="City*"><br>
-                        <input type="text" id="zipcode" name="zipcode" placeholder="Zipcode*"><br>
-                        <input type="text" id="phone" name="phone" placeholder="Phone"><br><br>
+                        <label for="fname">Billing and shippingadress</label><br />
+                        <input type="text" id="adress" name="adress" placeholder="Adress*" /><br />
+                        <input type="text" id="city" name="city" placeholder="City*" /><br />
+                        <input type="text" id="zipcode" name="zipcode" placeholder="Zipcode*" /><br />
+                        <input type="text" id="phone" name="phone" placeholder="Phone" /><br /><br />
 
-                        <label for="fname">Account details</label><br>
-                        <input type="text" id="mailadress" name="mailadress" placeholder="Email*"><br>
-                        <input type="password" id="password" name="password" placeholder="Password*"><br>
-                        <input type="password" id="verifypassword" name="verifypassword" placeholder="Verify your password*"><br>
+                        <label for="fname">Account details</label><br />
+                        <input type="text" id="mailadress" name="mailadress" placeholder="Email*" /><br />
+                        <input type="password" id="password" name="password" placeholder="Password*" /><br />
+                        <input type="password" id="verifypassword" name="verifypassword" placeholder="Verify your password*" /><br />
                         * Mandatory input.
-                        <br><br>
-                        <input type="submit" value="Create"><br>
+                        <br /><br />
+                        <input type="submit" value="Create" /><br />
                         By creating an account, you accept our <a href="#">Terms & Conditions</a>.
                     </form>
                 </div>
--- MarysMajesticMovies/ClientApp/src/components/NavMenu.js
+++ MarysMajesticMovies/ClientApp/src/components/NavMenu.js
@@ -39,6 +39,9 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/createUser">Create new account</NavLink>
+                </NavItem>
               </ul>
             </Collapse>
           </Container>
